ae2ad8802964027b4c5d8817b12bb10a635fc3e9
milkymetamouse <108646047+milkymetamouse@users.noreply.github.com>
2022-08-15 18:38:36 +1000

    Add files via upload

---
+++ javascript/sidebar.js
@@ -0,0 +1,20 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
